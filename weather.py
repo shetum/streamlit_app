206bc99b496cf1d253d6d38a5f7f8664a0a4fc85
shetum <56485247+shetum@users.noreply.github.com>
2024-02-23 21:14:27 +0300

    Add files via upload

---
+++ weather.py
@@ -0,0 +1,39 @@
+import streamlit as st
+
+import requests
+import json
+API_KEY = 'ceeb458124075676d8b43986e2514151'
+
+
+st.title("Погода")
+
+city = st.text_input("Введите название города", value="Калуга")
+
+def fetch_weather_data(city):
+    base_url = 'http://api.openweathermap.org/data/2.5/weather?'
+    complete_url = f"{base_url}appid={API_KEY}&q={city}"
+    response = requests.get(complete_url)
+    return response.json()
+ 
+def display_weather_data(weather_data):
+    if weather_data['cod'] != '404':
+        main_data = weather_data['main']
+        # Convert from Kelvin to Celsius
+        humidity = main_data['humidity']
+        temperature = main_data['temp'] - 273.15 
+        weather_description = weather_data['weather'][0]['description']
+
+        return round(temperature, 2)
+        print(f"Temperature: {temperature:.2f}°C")
+        print(f"Humidity: {humidity}%")
+        print(f"Weather description: {weather_description.capitalize()}")
+ 
+    else:
+        return("City not found. Please try again.")
+ 
+def main(city):
+
+    weather_data = fetch_weather_data(city)
+    return display_weather_data(weather_data)
+
+st.metric(label = f"Погода в {city}", value =  main(city))
